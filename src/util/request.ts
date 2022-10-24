11743b245f2fbeaad73f9437539b4eeb2a32125f
yud0uhu <cist.b219.y.osaka@gmail.com>
2022-10-25 08:00:26 +0900

    add: キーワード検索のエンドポイントを追加

--- src/util/request.ts
+++ src/util/request.ts
@@ -1,4 +1,10 @@
 export const requests =
   'http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=' +
-  process.env.REACT_APP_API_KEY +
+  // process.env.REACT_APP_API_KEY +
+  REACT_APP_API_KEY +
   '&format=json&large_area=Z041&middle_area=Y884';
+export const keyword_search_requests =
+  'http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=' +
+  // process.env.REACT_APP_API_KEY +
+  REACT_APP_API_KEY +
+  '&format=json&large_area=Z041&middle_area=Y884&keyword=';
